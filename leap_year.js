412303c48d5fb8d3f0e5234045cc70c6c58ab737
Nicholas <danielnick210@gmail.com>
2026-05-19 20:14:48 +0100

    created a function that checks weather a year is a leap year or not

---
+++ leap_year.js
@@ -0,0 +1,17 @@
+const leapYear = (num) => {
+	let leap = false;
+
+	if (isNaN(num)) {
+		console.log("please enter a valid year");
+		return false
+	}
+
+	if (num % 400 === 0) leap = true
+	else if (num % 100 === 0) return false
+	else if (num % 4 === 0) leap = true
+	else leap = false
+
+	return leap;
+};
+
+console.log(leapYear("abc"));
