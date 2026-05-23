e1c6039ef89f3322d2216f9d4972e08c66bf0509
Nicholas <danielnick210@gmail.com>
2026-05-23 09:41:52 +0100

    Update resplit.py and copy.js

--- test/copy.js
+++ test/copy.js
@@ -1,10 +1,10 @@
 let year = 2100
 
 if (year % 100 === 0)
-	if (year % 400 === 0) return "29 days"
-	else return "28 days"
+	if (year % 400 === 0) console.log("29 days");
+	else console.log("28 days");
 else
-	if (year % 4 === 0) return "29 days"
-	else return "28 days"
+	if (year % 4 === 0) console.log("29 days");
+	else console.log("28 days");
 
 // console.log(leap_year(2000))
