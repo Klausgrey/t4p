2876b424e87443bcd7bd2aea4fb141fc07aed068
Nicholas <danielnick210@gmail.com>
2026-05-25 21:09:12 +0100

    refactored variables

--- test/string_manipulation.js
+++ test/string_manipulation.js
@@ -1,4 +1,4 @@
-const manipulaltion = (string, commands) => {
+const manipulation = (string, commands) => {
 	if (string.length < 20)
 		return console.log("Please enter a sentence longer than 20 words");
 	if (commands.length < 1) return console.log("You entered an empty command");
@@ -6,21 +6,21 @@ const manipulaltion = (string, commands) => {
 	let back = [];
 	let arr = string;
 
-	for (let x of commands) {
-		if (x === "u") {
+	for (let command of commands) {
+		if (command === "u") {
 			arr = arr.toUpperCase();
 			back.push(arr);
-		} else if (x === "l") {
+		} else if (command === "l") {
 			arr = arr.toLowerCase();
 			back.push(arr);
-		} else if (x === "r") {
+		} else if (command === "r") {
 			arr = arr.split("").reverse().join("");
 			back.push(arr);
-		} else if (x === "z") {
+		} else if (command === "z") {
 			back.pop();
 			arr = back[back.length - 1];
-		} else if (x.includes("c ")) {
-			const [first, second, thrid] = x.split(" ");
+		} else if (command.includes("c ")) {
+			const [first, second, thrid] = command.split(" ");
 			arr = arr.replaceAll(second, thrid);
 			back.push(arr);
 		} else {
