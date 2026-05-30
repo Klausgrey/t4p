e92ec6ae11df13dab4bd2e338b4c1081b1a27964
Nicholas <danielnick210@gmail.com>
2026-05-30 14:32:14 +0100

    used the try and except to handle file input

--- new/index.py
+++ new/index.py
@@ -1,15 +1,17 @@
 import random
-countries = open("countries.txt", "r")
 data = []
-display = []
 
-for country in countries.readlines():
-	data.append(country.strip().lower())
+try:
+	with open("countries.txt", "r") as file:
+		data = [country.strip().lower() for country in file.readlines()]
+except FileNotFoundError:
+	print("No file found")
 
 def hangman():
+	display = []
 	guesses_words = []
 	random_countries = random.choice(data)
-	# random_countries = "El Salvador"
+	num_of_guesses = 3
 	print(random_countries)
 
 	if "{" in random_countries:
@@ -22,41 +24,42 @@ def hangman():
 			display.append("_")
 
 	print(" ".join(display))
-	num_of_guesses = 3
 
 	while True:
 		user_guess = input("Enter your guessed word: ").lower()
 		if user_guess == "exit":
 			print("Thanks for playing")
 			return
-		elif not user_guess.isalpha():
-			print("Enter a word not a number or special character ")
+
+		elif not user_guess.isalpha() or len(user_guess) != 1:
+			print("Invalid input! Enter a single letter (no numbers or special characters).")
 			continue
-		elif len(user_guess) < 1 or len(user_guess) > 1:
-			print("Enter just one word ")
-			continue
+
 		elif user_guess in guesses_words:
-			print("Enter a word you haven't entered before")
+			print("Enter a letter you haven't entered before")
 			continue
+
 		elif user_guess not in random_countries:
-			print(f"wrong guess, you have {num_of_guesses} remaining")
 			num_of_guesses -= 1
 			if num_of_guesses <= 0:
 				print(f"wrong guess, you have 0 remaining")
 				print("you've exhausted all your guesses")
 				return
+
+			print(f"wrong guess, you have {num_of_guesses} remaining")
 			continue
+
 		else:
 			for index, letter in enumerate(random_countries):
 				if letter == user_guess:
 					display[index] = user_guess
 					guesses_words.append(user_guess)
-					print(display)
-			continue
 
-		# return print(display)
+			print("".join(display))
 
-
+			if "_" not in display:
+				print("congarts you won")
+				return
 hangman()
 
 
