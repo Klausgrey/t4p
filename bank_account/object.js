57af881cab3ee6152e0040cefe404486b47e2d38
Nicholas <danielnick210@gmail.com>
2026-06-01 07:04:14 +0100

    week7 assignment t4p

---
+++ bank_account/object.js
@@ -0,0 +1,32 @@
+class BankAccount {
+	constructor(balance) {
+		if (balance < 0) throw new Error("balance cannot be negative");
+		this.balance = balance;
+	}
+
+	creditAmount(amount) {
+		this.balance = this.balance + amount
+	}
+
+	debitAmount(amount) {
+		if (typeof amount !== "number" || isNaN(amount) || amount <= 0) {
+			return console.log("Invalid amount. Please enter a positive number.");
+		}
+		if (amount > this.balance) {
+			return console.log("Debit amount exceeds account balance.");
+		}
+
+		this.balance = this.balance - amount;
+	}
+
+	getBalance() {
+		return this.balance;
+	}
+}
+
+
+const user1 = new BankAccount(0)
+// console.log(user1.balance);
+user1.creditAmount(2000)
+user1.debitAmount(500);
+console.log(user1.getBalance())
