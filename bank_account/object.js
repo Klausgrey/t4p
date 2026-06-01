93442bff741cfa5d58b0925657da094c774af5f7
Nicholas <danielnick210@gmail.com>
2026-06-01 21:14:54 +0100

    added the same guard to credit

--- bank_account/object.js
+++ bank_account/object.js
@@ -5,6 +5,13 @@ class BankAccount {
 	}
 
 	creditAmount(amount) {
+		if (typeof amount !== "number" || isNaN(amount) || amount <= 0) {
+			return console.log("Invalid amount. Please enter a positive number.");
+		}
+		if (amount > this.balance) {
+			return console.log("Debit amount exceeds account balance.");
+		}
+
 		this.balance = this.balance + amount
 	}
 
@@ -27,11 +34,11 @@ class BankAccount {
 const user1 = new BankAccount(0)
 
 try {
-	user1.creditAmount(2000)
-	user1.debitAmount(100);
+	user1.creditAmount("100")
+	user1.debitAmount("99");
 	console.log(user1.getBalance())
 
 } catch (err) {
-	console.log(err);
+	console.log(err.massage);
 }
 // console.log(user1.balance);
