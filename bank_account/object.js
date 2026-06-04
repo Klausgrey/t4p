bc20ba3018bf1b87290dd9dfeed78fac04b1b771
Nicholas <danielnick210@gmail.com>
2026-06-04 08:25:36 +0100

    learnt the sliding window tecnie

--- bank_account/object.js
+++ bank_account/object.js
@@ -1,14 +1,21 @@
 class BankAccount {
 	constructor(balance) {
-		if (balance < 0 || balance === null || balance === undefined) {
-			throw new Error("Balance cannot be negative");
+		if (typeof balance !== "number" || isNaN(balance)) {
+			this.balance = balance;
+		} else if (balance < 0 || balance === null || balance === undefined) {
+			this.balance = 0;
+			console.log("Invalid");
+		} else if (balance >= 0) {
+			this.balance = 0;
+			console.log("Invalid amount. Please enter a positive number.");
+			return;
 		}
-		this.balance = balance;
 	}
 
 	creditAmount(amount) {
 		if (typeof amount !== "number" || isNaN(amount) || amount <= 0) {
-			return console.log("Invalid amount. Please enter a positive number.");
+			console.log("Invalid amount. Please enter a positive number.");
+			return;
 		}
 
 		this.balance = this.balance + amount;
@@ -16,10 +23,12 @@ class BankAccount {
 
 	debitAmount(amount) {
 		if (typeof amount !== "number" || isNaN(amount) || amount <= 0) {
-			return console.log("Invalid amount. Please enter a positive number.");
+			console.log("Invalid amount. Please enter a positive number.");
+			return;
 		}
 		if (amount > this.balance) {
-			return console.log("Debit amount exceeds account balance.");
+			console.log("Debit amount exceeds account balance.");
+			return;
 		}
 
 		this.balance = this.balance - amount;
@@ -30,8 +39,8 @@ class BankAccount {
 	}
 }
 
-const user1 = new BankAccount(-1000);
-user1.creditAmount("100");
-user1.debitAmount(99);
+const user1 = new BankAccount("2000");
+user1.creditAmount(200);
+user1.debitAmount(400);
 console.log(user1.getBalance());
 // console.log(user1.balance);
