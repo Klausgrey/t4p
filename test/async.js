e1ba8e2c9ec4cc88c2589cc4d194a204c743bebb
Nicholas <danielnick210@gmail.com>
2026-06-06 18:56:11 +0100

    learning promises

---
+++ test/async.js
@@ -0,0 +1,41 @@
+// console.log("Start");
+
+// // setInterval(() => {
+// // 	console.log("This should display every 2 seconds");
+// // }, 2000)
+
+// const display = () => {
+// 	console.log("This should show at exactly 8 seconds");
+// };
+// const myFunction = () => {
+// 	let d = new Date();
+// 	console.log(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+// }
+
+// setTimeout(function () {
+// 	myFunction();
+// }, 60000);
+
+// console.log("End");
+
+// const display = (something) => {
+// 	console.log(something);
+// };
+
+// const calculateSum = (num1, num2) => {
+// 	let sum = num1 + num2;
+// 	display(sum);
+// };
+
+// calculateSum(5, 6);
+
+// const display = (something) => {
+// 	console.log(something);
+// };
+
+// const calculateSum = (num1, num2, mycallback) => {
+// 	let sum = num1 + num2
+// 	mycallback(sum)
+// }
+
+// calculateSum(5, 5, display)
