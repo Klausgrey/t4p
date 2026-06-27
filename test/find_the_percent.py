edf440dc3234d07c845604b3b700941f3c14c7e3
Nicholas <danielnick210@gmail.com>
2026-06-27 22:12:18 +0100

    week-5-assignment

--- test/find_the_percent.py
+++
@@ -1,9 +0,0 @@
-student_marks = dict(alpha = [20,30,40], beta = [30,50,70])
-query_name = "beta"
-
-value = student_marks[query_name]
-
-average = (sum(value) / len(value))
-print(f"{average:.2f}")
-# print(f"\'\"This is Edosa's code\'\"")
-
--- test/h.py
+++
@@ -1,5 +0,0 @@
-thickness = 5
-
-for i in range(1, 6):
-	(2 * 1 - 1) #decreases the count manually
-	print(('H' * (2 * i - 1)).center(thickness * 5))
