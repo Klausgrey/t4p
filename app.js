697272501cb4b7823d5091cb32d49e9b4ff783c7
Nicholas <danielnick210@gmail.com>
2026-07-18 16:43:19 +0100

    created the verify token function

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ import authRouter from "./src/routers/auth.router.js";
 const app = express();
 
 app.use(express.json())
-app.use("/", authRouter);
+app.use("/auth", authRouter);
 
 export default app;
---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import Jwt from "jsonwebtoken";
+import "dotenv/config";
+const jwt = Jwt;
+
+export const verifytoken = (req, res, next) => {
+	try {
+		const header = req.headers["authorization"];
+		if (!header) res.status(401).json({ error: "token was not provided..." });
+
+		const token = header.split(" ")[1];
+		if (!token) res.status(401).json({ error: "access token required" });
+
+		const user = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = user;
+	} catch (err) {
+		if (error.name === "TokenExpiredError")
+			return res.status(401).json({ error: "Token has expired..." });
+		if (error.name === "JsonWebTokenError")
+			return res.status(403).json({ error: "Invalid token..." });
+		return res.status(500).json({ error: "Token verification failed" });
+	}
+};
