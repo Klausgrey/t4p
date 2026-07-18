41758de4cbe71532fd42f9b65788f2b59c035d24
Nicholas <danielnick210@gmail.com>
2026-07-18 17:54:30 +0100

    connected the route and app.js file

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import authRouter from "./src/routers/auth.router.js";
+import noteRouter from "./src/routers/note.router.js";
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use("/auth", authRouter);
+app.use("/user", noteRouter);
 
 export default app;
--- src/routers/note.router.js
+++ src/routers/note.router.js
@@ -1,2 +1,10 @@
-
+import express from "express";
+import { create, getNote } from "../controllers/note.controller.js";
 import { verifytoken } from "../middleware/authMiddleware.js";
+import { get } from "node:http";
+
+const router = express.Router();
+router.post("/", verifytoken, create);
+router.get("/", verifytoken, getNote);
+
+export default router;
