bd013759d7be603d53d5ebcad52058eaf22c4637
Nicholas <danielnick210@gmail.com>
2026-07-18 17:50:29 +0100

    created the notes controllers

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,4 +1,26 @@
 import { createNote, getUserNote } from "../models/noteStore.js";
 
+export const create = async (req, res) => {
+	const userId = req.user.id;
+	const { title, body, tag } = req.body;
 
-export const 
+	try {
+		const data = createNote(userId, title, body, tag);
+		res.status(201).json({
+			message: "note created successfully...",
+			data,
+		});
+	} catch (err) {
+		res.status(500).json({ error: "Failed to create note" });
+	}
+};
+
+export const getNote = async (req, res) => {
+	const userId = req.user.id;
+	try {
+		const data = getUserNote(userId);
+		res.status(200).json({ status: "success", data });
+	} catch (err) {
+		res.status(500).json({ error: "Failed to fetch notes" });
+	}
+};
