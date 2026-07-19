840b74c47d038e94e3e8fff63a9ff0d6b491a87c
Nicholas <danielnick210@gmail.com>
2026-07-19 03:01:14 +0100

    plugged in the delete control to the router

--- src/routers/note.router.js
+++ src/routers/note.router.js
@@ -4,14 +4,17 @@ import {
 	UserNote,
 	getNotes,
 	patchNote,
+	deleteNote
 } from "../controllers/note.controller.js";
 import { verifytoken } from "../middleware/authMiddleware.js";
 import { get } from "node:http";
+import { validate } from "uuid";
 
 const router = express.Router();
 router.post("/", verifytoken, create);
 router.get("/", verifytoken, UserNote);
 router.get("/all", verifytoken, getNotes);
 router.patch("/:id", verifytoken, patchNote);
+router.delete("/:id", validate, deleteNote)
 
 export default router;
